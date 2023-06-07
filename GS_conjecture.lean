3c19cf97ddc8924393bebbfc2dd485f2ca19423c
Sean O'Connor <seanoconnor2019@gmail.com>
2023-06-06 23:16:58 -0400

    Finished eq_Zero, eq_One, and eval and map commute lemmas

--- GS_conjecture.lean
+++ GS_conjecture.lean
@@ -5,30 +5,80 @@ import Mathlib.Data.Rat.Cast
 
 open MvPolynomial
 
-universe u
+universe u v
 
-def FinProduct (R : Type u) [Field R] : (n : ℕ) → (Fin n → R) → R := fun
+def FinProduct (R : Type u) [CommSemiring R] : (n : ℕ) → (Fin n → R) → R := fun
   | .zero => λ _ => 1
   | .succ n => λ f => (f 0) * (FinProduct R n (f ∘ Fin.succ)) 
 
--- checking the above definition by implementing the factorial
+-- checking the above definition by implementing the factorial function
 -- #eval FinProduct ℚ 5 (λ k => 1 + (Fin.valEmbedding k : ℚ))
 
-lemma FinProductZero (R : Type u) [Field R] : (n : ℕ) → (f : Fin n → R)
-  → (k : Fin n) → (f k = 0) → (FinProduct R n f) = 0 := by
-    intro n
+lemma FinProductZero {R : Type u} [CommSemiring R] {f : Fin 0 → R} 
+  : FinProduct R 0 f = 1 := by
+  constructor
+
+lemma FinProductSucc {R : Type u} [CommSemiring R] {n : ℕ} {f : Fin (n + 1) → R} 
+  : FinProduct R (n + 1) f = (f 0) * (FinProduct R n (f ∘ Fin.succ)) := by
+  constructor
+
+lemma FinProduct_eq_Zero {R : Type u} [CommSemiring R] [NoZeroDivisors R] {n : ℕ} 
+  {f : Fin n → R} {k : Fin n} {hk : f k = 0} : (FinProduct R n f) = 0 := by
     induction n with
-    | zero => rintro _ ⟨k, hk'⟩
+    | zero => cases' k with k hk'
               exact (Nat.not_lt_zero _ hk').elim
-    | succ n hn =>  rintro f ⟨k, hk'⟩ hk 
-                    unfold FinProduct
+    | succ n hn =>  cases' k with k hk' 
+                    rw[FinProductSucc]
                     rw[mul_eq_zero]
                     cases k with
                     | zero => left
                               exact hk
                     | succ k => right
-                                apply hn (f ∘ Fin.succ) 
+                                apply @hn (f ∘ Fin.succ) 
                                   ⟨k, Nat.lt_of_succ_lt_succ hk'⟩
                                 rw[Function.comp_apply, Fin.succ_mk]
                                 exact hk
+
+lemma FinProduct_eq_One {R : Type u} [CommSemiring R] {n : ℕ} 
+  {f : Fin n → R} {hf : ∀ (k : Fin n), f k = 1} : (FinProduct R n f) = 1 := by
+    induction n with
+    | zero => exact FinProductZero
+    | succ n hn => rw[FinProductSucc]
+                   rw[hf 0]
+                   rw[one_mul]
+                   rw[hn]
+                   intro k
+                   rw[Function.comp_apply]
+                   exact hf (Fin.succ k)
+                   
+lemma EvalFinProduct_eq_FinProductEval {R : Type u} [CommSemiring R] 
+  {m n : ℕ} {f : Fin n → MvPolynomial (Fin m) R} {g : Fin m → R} :
+  eval g (FinProduct (MvPolynomial (Fin m) R) n f)
+  = FinProduct R n ((eval g) ∘ f) := by
+    induction n with
+    | zero => repeat rw[FinProductZero]
+              rw[map_one]
+    | succ n hn => repeat rw[FinProductSucc]
+                   rw[map_mul]
+                   rw[Function.comp_apply]
+                   rw[hn]
+                   rfl
+
+lemma MapFinProduct_eq_FinProductMap {R : Type u} [CommSemiring R] {S : Type v} 
+  [CommSemiring S] {m n : ℕ} {f : Fin n → MvPolynomial (Fin m) R} {g : R →+* S} :
+  map g (FinProduct (MvPolynomial (Fin m) R) n f)
+  = FinProduct (MvPolynomial (Fin m) S) n ((map g) ∘ f) := by 
+    induction n with
+    | zero => repeat rw[FinProductZero]
+              rw[map_one]
+    | succ n hn => repeat rw[FinProductSucc]
+                   rw[map_mul]
+                   rw[Function.comp_apply]
+                   rw[hn]
+                   rfl
+
+
+
+
+
 
